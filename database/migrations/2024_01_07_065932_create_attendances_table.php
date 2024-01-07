54c53069e05f25ea547194643241dc14f32a3546
Amila T Kumarasekara <91652718+Yenaya-L-Kumarasekara@users.noreply.github.com>
2024-01-07 14:56:54 +0530

    Create database migration tables for the server according to the er diagram

---
+++ database/migrations/2024_01_07_065932_create_attendances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id('attendance_id');
+            $table->dateTime('check_in')->nullable();
+            $table->dateTime('check_out')->nullable();
+            $table->foreignId('employee_id')->constrained();
+            $table->foreignId('schedule_id')->constrained()->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
